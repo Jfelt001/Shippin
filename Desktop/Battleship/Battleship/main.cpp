e0fc67b9a3226a761d0cdd62b0f8e72017a5fbf9
Jared Felton <jaredfelton@Jareds-Air.frontierlocal.net>
2018-11-03 13:17:36 -0700

    Adding Battleship

---
+++ Desktop/Battleship/Battleship/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Battleship
+//
+//  Created by Jared Felton on 11/3/18.
+//  Copyright © 2018 Jared Felton. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
