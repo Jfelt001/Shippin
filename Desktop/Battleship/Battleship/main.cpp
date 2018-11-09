3be71ec29fb2e75c2f0d180568f9fd971496f7d2
Jared Felton <jaredfelton@jareds-macbook-air.local>
2018-11-08 20:49:53 -0800

    Binary Search

--- Desktop/Battleship/Battleship/main.cpp
+++ Desktop/Battleship/Battleship/main.cpp
@@ -9,7 +9,31 @@
 #include <iostream>
 
 using namespace std;
+int binarySearch(int arr[], int l, int r, int x) {
+    int mid = l + (r - l)/2;
+    
+if (mid == x)
+    return x;
+    
+if (mid > x)
+    r = mid - l;
+    
+else l = mid - r;
+    
+    return -1;
+    
+}
 int main(int argc, char **argv)
 {
-    cout<<"hello";
+   int arr[] = {2, 3, 4, 10, 40, 53, 77, 94, 104, 204, 555, 1000};
+    int binary = 4;
+    int l = 0;
+    int r = 11;
+    int x = binarySearch(arr, l, r, binary);
+    cout<<x;
+    
+    
+    
 }
+
+
