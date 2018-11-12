9e019d92fd28c6a39accd2fd899dcd31a1e008a1
Jared Felton <jaredfelton@jareds-macbook-air.local>
2018-11-11 20:02:17 -0800

    Finished!

--- Desktop/Battleship/Battleship/main.cpp
+++ Desktop/Battleship/Battleship/main.cpp
@@ -10,27 +10,32 @@
 
 using namespace std;
 int binarySearch(int arr[], int l, int r, int x) {
+    while (l != r) {
+    
     int mid = l + (r - l)/2;
     
-if (mid == x)
+if (arr[mid] == x)
     return x;
     
-if (mid > x)
+if (arr[mid] > x)
     r = mid - l;
     
 else l = mid - r;
-    
+    }
     return -1;
     
 }
 int main(int argc, char **argv)
 {
    int arr[] = {2, 3, 4, 10, 40, 53, 77, 94, 104, 204, 555, 1000};
-    int binary = 4;
+    int find = 10;
     int l = 0;
     int r = 11;
-    int x = binarySearch(arr, l, r, binary);
-    cout<<x;
+    int result = binarySearch(arr, l, r, find);
+    if (result == -1)
+        cout<<"That number is not in this list."<<endl;
+    else
+        cout<<result<<endl;
     
     
     
